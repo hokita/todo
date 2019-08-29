166fb1024edb39c77ed1283a3e8255cd29434014
hokita <hideee.0202@gmail.com>
2019-08-29 09:48:13 +0900

    mod settings

--- app/javascript/components/hello_react.jsx
+++ app/javascript/components/hello_react.jsx
@@ -18,9 +18,11 @@ Hello.propTypes = {
   name: PropTypes.string
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(
+//     <Hello name="React" />,
+//     document.body.appendChild(document.createElement('div')),
+//   )
+// })
+
+export default Hello
