620384956e3f66e56e57d8f6e26098c2a4ccddaa
hokita <hideee.0202@gmail.com>
2019-09-25 10:05:33 +0900

    add tasks create method

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -5,7 +5,25 @@ module Api
     # todoのリストを操作するコントローラー
     class TasksController < ApplicationController
       def index
-        render json: { hoge: 'hoge' }
+        render json: { hoge: 'hoge' },
+               status: :ok
+      end
+
+      def create
+        task = Task.new(task_params)
+        if task.save
+          render json: { hoge: 'hoge' },
+                 status: :created
+        else
+          render json: { error: 'Failed to creation.' },
+                 status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def task_params
+        params.fetch(:task, {})
       end
     end
   end
